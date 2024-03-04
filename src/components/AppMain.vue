5753c9b9c67c9274b48848bc40e9e8053781f1bd
Zariy7 <nazariy.hereha@gmail.com>
2024-03-04 09:26:44 +0100

    front office

---
+++ src/components/AppMain.vue
@@ -0,0 +1,28 @@
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'AppMain',
+  data(){
+    return {
+      projects: [],
+    }
+  },
+  created(){
+    this.getProjects();
+  },
+  methods: {
+    getProjects(){
+      axios.get(`http://127.0.0.1:8000/api/projects`).then((response) =>{
+        console.log(response.data.results);
+      });
+    }
+  }
+}
+</script>
+
+<template>
+</template>
+
+<style scoped>
+</style>
